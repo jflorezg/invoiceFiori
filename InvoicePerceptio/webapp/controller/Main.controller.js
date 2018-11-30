b8ad191ebe1135a3be6193d33e00ca352335290e
jflorezg <jflorezg@users.noreply.github.com>
2018-11-29 20:17:03 -0500

    Initial commit
    Initial commit

---
+++ InvoicePerceptio/webapp/controller/Main.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"com/perceptio/invoiceperceptio/InvoicePerceptio/controller/BaseController"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.perceptio.invoiceperceptio.InvoicePerceptio.controller.Main", {
+
+	});
+});
